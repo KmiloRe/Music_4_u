f58bdea8d383d39f44dac4858be39e25fb922aec
Camilo Restre <camilorestrerodriguez@gmail.com>
2024-04-15 16:44:27 -0500

    feat:images almost ready

--- js/data.js
+++ js/data.js
@@ -1,37 +1,65 @@
 let songs = [
     {
-        name: 'Loquita',
+        name: 'Darte un beso',
+        path: 'musics/Song 5.mp3',
+        artist: 'Prince Royce',
+        cover: 'img/marimari.png',
+        duracion: '3:00'
+    },
+    {
+        name: 'Azul',
         path: 'musics/Song 1.mp3',
-        artist: 'Randy',
-        cover: 'img/Randy.png',
-        duracion: '03:00'
+        artist: 'Zoe',
+        cover: 'img/Zoe.png',
+        duracion: '3:51'
     },
     {
         name: 'Luna',
         path: 'musics/Song 2.mp3',
         artist: 'Feid',
-        cover: 'img/Luna.png',
+        cover: 'img/encampo.png',
         duracion: '3:00'
     },
     {
-        name: 'La Ex',
+        name: 'Zombie',
         path: 'musics/Song 3.mp3',
-        artist: 'Luis Alfonso',
-        cover: 'img/luis.png',
-        duracion: '3:00'
+        artist: 'The Cranberries',
+        cover: 'img/feriaaerea.png',
+        duracion: '5:08'
     },
     {
         name: 'Propuesta Indecente',
         path: 'musics/Song 4.mp3',
         artist: 'Romeo Santos',
-        cover: 'img/Romeo.png',
+        cover: 'img/ciego.png',
         duracion: '3:00'
     },
     {
-        name: 'Darte un beso',
+        name: 'Ojos',
         path: 'musics/Song 5.mp3',
         artist: 'Prince Royce',
-        cover: 'img/Prince.png',
+        cover: 'img/ojos.png',
+        duracion: '3:00'
+    },
+    {
+        name: 'Boca',
+        path: 'musics/Song 5.mp3',
+        artist: 'Prince Royce',
+        cover: 'img/boca.png',
+        duracion: '3:00'
+    },
+    {
+        name: 'oso',
+        path: 'musics/Song 5.mp3',
+        artist: 'Prince Royce',
+        cover: 'img/oso.png',
+        duracion: '3:00'
+    },
+    {
+        name: 'Cocina',
+        path: 'musics/Song 5.mp3',
+        artist: 'Prince Royce',
+        cover: 'img/cocina.png',
         duracion: '3:00'
     },
 ]
